2d576ed45f4d89c9e7ec4519983648667d9a4c63
Spencer Gass <sgass@kepler.space>
2025-03-29 21:34:54 -0400

    Add comments to hello_world.c

--- source/hello_world.c
+++ source/hello_world.c
@@ -1,19 +1,23 @@
 #include <linux/module.h>
 
+// Called when module is loaded
 static int __init m_init(void)
 {
 	printk(KERN_ALERT "Hello, world!\n");
 	return 0;
 }
 
+// Called when module is unloaded
 static void __exit m_exit(void)
 {
 	printk(KERN_ALERT "Bye, world!\n");
 }
 
+// Registers the init and exit functions
 module_init(m_init);
 module_exit(m_exit);
 
+// Module Metadata
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Spencer Gass");
 MODULE_DESCRIPTION("Hello World program");
