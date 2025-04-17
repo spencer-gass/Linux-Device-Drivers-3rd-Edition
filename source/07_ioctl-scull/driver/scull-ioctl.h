3f2abe448eefe4f5e607320cb68b652ab06d3900
Spencer Gass <sgass@kepler.space>
2025-04-17 17:21:53 -0400

    Implement reset, status, and append ioctl commands.

--- source/07_ioctl-scull/driver/scull-ioctl.h
+++ source/07_ioctl-scull/driver/scull-ioctl.h
@@ -5,10 +5,13 @@
 
 #define IOCTL_RESET	    _IO(IOCTL_IOC_MAGIC, 0)
 #define IOCTL_STATUS    _IOR(IOCTL_IOC_MAGIC, 1, struct ioctl_arg*)
-#define IOCTL_APPEND    _IOW(IOCTL_IOC_MAGIC, 2, struct ioctl_arg*)
+#define IOCTL_WRITE    _IOW(IOCTL_IOC_MAGIC, 2, struct ioctl_arg*)
 
 #define IOCTL_MAXNR		2
-#define MAX_IOCTL_MSG_SIZE 1024
+#define MAX_IOCTL_MSG_SIZE 128
+
+#define IS_KERNEL_BUF   0
+#define IS_USER_BUF	    1
 
 struct ioctl_arg {
     int len;
