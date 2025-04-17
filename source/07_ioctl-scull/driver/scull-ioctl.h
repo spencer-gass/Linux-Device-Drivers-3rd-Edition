1ad4e18775205ddc6f485e035f46252be6594c1d
Spencer Gass <sgass@kepler.space>
2025-04-16 21:24:06 -0400

    Pass arg struct from user to ioctl through ioctl.

--- source/07_ioctl-scull/driver/scull-ioctl.h
+++ source/07_ioctl-scull/driver/scull-ioctl.h
@@ -4,8 +4,8 @@
 #define IOCTL_IOC_MAGIC		'd'
 
 #define IOCTL_RESET	    _IO(IOCTL_IOC_MAGIC, 0)
-#define IOCTL_STATUS    _IOR(IOCTL_IOC_MAGIC, 1, char*)
-#define IOCTL_APPEND    _IOW(IOCTL_IOC_MAGIC, 2, char*)
+#define IOCTL_STATUS    _IOR(IOCTL_IOC_MAGIC, 1, struct ioctl_arg*)
+#define IOCTL_APPEND    _IOW(IOCTL_IOC_MAGIC, 2, struct ioctl_arg*)
 
 #define IOCTL_MAXNR		2
 
